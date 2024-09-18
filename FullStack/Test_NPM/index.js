8ca2d6a7f7f09152db9c8ea2a04bbd0655cf09d2
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-09-18 13:15:15 +0300

    Lesson3

--- FullStack/Test_NPM/index.js
+++ FullStack/Test_NPM/index.js
@@ -3,9 +3,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+app.get('/list', (req, res) => {
+    res.send('Listing data from a file!');
+});
+
+//Oletusreitti, joka tarjoillaan, mikäli muihin ei päädytty.
+app.get('*', (req, res) => {
+    res.send('Can not find the requested page', 404);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
